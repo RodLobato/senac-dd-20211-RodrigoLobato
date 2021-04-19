525cad7953049b66f1e594abdbc37cfc3b4f80f5
Rodrigo Lobato <rodlobatobass@gmail.com>
2021-04-18 23:06:25 -0300

    Interação com banco de dados, DAO, e VIEWS

---
+++ Vaccine_dd_Senac/src/com/br/lobato/db/DbException_Vaccine.java
@@ -0,0 +1,11 @@
+package com.br.lobato.db;
+
+public class DbException_Vaccine  extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbException_Vaccine(String msg) {
+		super(msg);
+	}
+
+}
